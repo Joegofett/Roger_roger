5433f3539c5b994f9f5a556475d96df39bf6e4a5
joegofett <joegofett@system76-pc.localdomain>
2020-10-10 10:33:01 -0400

    adding some code and working on it still. Took out api code and will deal with secrets after I get the poll working

--- ingestor.py
+++ ingestor.py
@@ -1,1 +1,24 @@
+import requests
 import discord
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author ==client.user:
+        return
+
+    if message.content.startswith('$help'):
+        await message.channel.send("""Roger_roger \n 
+        What do I do?
+        Currently I make Polls\n
+        How do I do this?
+        $poll [Question]\n [Option1], [Option2], [Option3]\n
+        Where is my code?
+        github.com/joegofett
+        """)
+
+    if message.content.startswith('$poll %s '.):
+        await message.channel.send(""" Test""")
+
+client.run('')
